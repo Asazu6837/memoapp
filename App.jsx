6bf377c557110bb933bdae55c92de65c0ce021e8
Asazu6837 <145944795+Asazu6837@users.noreply.github.com>
2023-09-28 12:02:02 +0900

    case 64 half

--- App.jsx
+++ App.jsx
@@ -1,47 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 
+import AppBar from "./src/compornents/AppBar";
+import MemoList from "./src/compornents/MemoList";
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.appbar}>
-        <View style={styles.appbarInner}>
-          <Text style={styles.appbarTitle}>Memo App</Text>
-          <Text style={styles.appbarRight}>ログアウト</Text>
-        </View>
-      </View>
 
-      <View>
-        <View style={styles.memoListItem}>
-         <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memolistItemData}>2020年12月24日</Text>
-          </View>
-          <View>
-           <Text>X</Text>
-          </View>
-        </View>
-
-        <View style={styles.memoListItem}>
-         <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memolistItemData}>2020年12月24日</Text>
-          </View>
-          <View>
-           <Text>X</Text>
-          </View>
-        </View>
-
-        <View style={styles.memoListItem}>
-         <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memolistItemData}>2020年12月24日</Text>
-          </View>
-          <View>
-           <Text>X</Text>
-          </View>
-        </View>
-
-      </View>
+      <AppBar />
+      <MemoList />
 
       <View style={styles.circleButton}>
         <Text style={styles.circleButtonLabel}>+</Text>
@@ -55,54 +22,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F0F4F8',
-  },
-
-  appbar: {
-    width: '100%',
-    height: 104,
-    backgroundColor: '#467FD3',
-    justifyContent: 'flex-end',
-  },
-
-  appbarInner: {
-    alignItems: 'center',
-  },
-
-  appbarRight: {
-    position: 'absolute',
-    right: 19,
-    bottom: 16,
-    color: 'rgba(255, 255, 255, 0.8)',
-  },
-
-  appbarTitle: {
-    marginBottom: 8,
-    fontSize: 22,
-    lineHeight: 24,
-    color: '#fff',
-    fontWeight: 'bold',
-  },
-
-  memoListItem: {
-    backgroundColor: '#fff',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingVertical: 16,
-    paddingHorizontal: 19,
-    alignItems: 'center',
-    borderBottomWidth: 1,
-    borderColor: 'rgba{0, 0, 0, 0.15}',
-  },
-
-  memoListItemTitle: {
-    fontSize: 16,
-    lineHeight: 32,
-  },
-
-  memolistItemData: {
-    fontSize: 12,
-    lineHeight: 16,
-    color: '#848484',
   },
 
   circleButton: {
