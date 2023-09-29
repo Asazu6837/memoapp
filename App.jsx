e1f4aa7d9e72e27e5d3436a36720880b5ec861b9
Asazu6837 <145944795+Asazu6837@users.noreply.github.com>
2023-09-29 10:33:04 +0900

    case64 finish

--- App.jsx
+++ App.jsx
@@ -1,49 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 
-import AppBar from "./src/compornents/AppBar";
-import MemoList from "./src/compornents/MemoList";
+import MemoListScreen from "./src/screens/MemoListScreen";
+import MemoDetailScreen from "./src/screens/MemoDetailScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-
-      <AppBar />
-      <MemoList />
-
-      <View style={styles.circleButton}>
-        <Text style={styles.circleButtonLabel}>+</Text>
-      </View>
-    </View>
+    <MemoDetailScreen />
   );
 }
-
-// item
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F0F4F8',
-  },
-
-  circleButton: {
-    backgroundColor: '#467FD3',
-    width: 64,
-    height: 64,
-    borderRadius: 32,
-    justifyContent:'center',
-    alignItems: 'center',
-    position: 'absolute',
-    right: 40,
-    bottom: 40,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 8 },
-    shadowOpacity: 0.25,
-    shadowRadius: 8,
-    elevation: 8,
-  },
-
-  circleButtonLabel: {
-    color: '#fff',
-    fontSize: 40,
-    lineHeight: 40,
-  },
-});
---
+++ src/screens/MemoListScreen.jsx
@@ -0,0 +1,22 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+import AppBar from '../compornents/AppBar';
+import MemoList from '../compornents/MemoList';
+import CircleButton from '../compornents/CircleButton';
+
+export default function MemoListScreen() {
+    return(
+        <View style={styles.container}>
+        <AppBar />
+        <MemoList />
+        <CircleButton>+</CircleButton>
+      </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F0F4F8',
+      }
+});
